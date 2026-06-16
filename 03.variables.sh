eadce17729093c218a4ba72a9ac5d5d2b7a628be
Anusha09242 <anushapothu302@gmail.com>
2026-06-16 13:23:09 +0530

    variables

--- 03.variables.sh
+++ 03.variables.sh
@@ -5,4 +5,4 @@ PERSON2=$2
 echo "$PERSON1: Hi $PERSON2, how are you today?"
 echo "$PERSON2: I'm doing well, $PERSON1. How about you?"
 echo "$PERSON1: I'm good too. Are you free this evening?"
-echo "$PERSON2: "Yes, I am. Let's catch up after work." 
+echo "$PERSON2: Yes, I am. Let's catch up after work." 
