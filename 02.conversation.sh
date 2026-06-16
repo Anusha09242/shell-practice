d0dee52599f99acc4bf496dd246ea4415272da54
Anusha09242 <anushapothu302@gmail.com>
2026-06-16 12:52:01 +0530

    added variables

--- 02.conversation.sh
+++ 02.conversation.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "Rahul: Hi Priya, how are you today?"
-echo "Shiva: I'm doing well, Rahul. How about you?"
-echo "Rahul: I'm good too. Are you free this evening?"
-echo "Shiva: Yes, I am. Let's catch up after work." 
+PERSON1=Rahul
+PERSON2=Shiva
+echo "$PERSON1: Hi $PERSON2, how are you today?"
+echo "$PERSON2: I'm doing well, $PERSON1. How about you?"
+echo "$PERSON1: I'm good too. Are you free this evening?"
+echo "$PERSON2: Yes, I am. Let's catch up after work." 
