70c5164c6a57b3cc9159d392eb0531cae09fb710
Anusha09242 <anushapothu302@gmail.com>
2026-06-16 13:47:44 +0530

    variables

--- 05.variables.sh
+++ 05.variables.sh
@@ -2,4 +2,14 @@
 
 TIMESTAMP=$(date)
 
-echo "Time is: $TIMESTAMP"
+# echo "Time is: $TIMESTAMP"
+
+STARTTIME=$(date +%s)
+
+sleep 10
+
+ENDTIME=$(date +%s)
+
+TOTALTIME=$(($STARTTIME-$ENDTIME))
+
+echo "The total time is $TOTALTIME seconds"
