ebc287bca86fd0240e84378a02ae81f3f52501a0
Anusha09242 <anushapothu302@gmail.com>
2026-06-21 14:44:41 +0530

    special variables

--- 06.special_variables.sh
+++ 06.special_variables.sh
@@ -3,3 +3,16 @@
 #### Special Vars ####
 echo "All variables passed to script: $@"
 echo "Number of variables passsed: $#"
+echo "First variable: $1"
+echo "Script name: $0"
+echo "who is running this: $USER"
+echo "which directory: $PWD"
+echo "Home directory: $HOME"
+echo "PID of the current script: $$"
+sleep 5 &
+echo "PID of the background command running just now: $!"
+wait $!
+echo "Line number: $LINENO"
+echo "Script executed in $SECONDS seconds"
+echo "Random number $RANDOM"
+echo "Exit code of previous command: $?"
