cf05344fdad95a9fb2033d46b5de2f8465a77f36
Anusha09242 <anushapothu302@gmail.com>
2026-06-21 15:14:06 +0530

    data types

--- 07.data-types.sh
+++ 07.data-types.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-NUM1=10
+NUM1=20
 #NUM2=20
-NUM2=linux
+NUM2=linux #as it is not a number, default it is considered as 0
 SUM=$(($NUM1+$NUM2))
 
 echo "Sum is: $SUM"
+
+#ARRAY
+
+MOVIES=("RRR" "Varanasi" "Pushpa")
+echo "Movies are: ${MOVIES[@]}"
+echo "First movie is: ${Movies[0]}"
+echo "Second movie is: ${Movies[1]}"
+echo "Third movie is: ${Movies[2]}"
