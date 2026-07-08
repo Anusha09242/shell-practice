400f7759c680fcadea14cabe3a2babd7b03ab10a
Anusha09242 <anushapothu302@gmail.com>
2026-07-08 11:56:59 +0530

    Disk usage alert

--- mail.sh
+++ mail.sh
@@ -7,7 +7,7 @@ MESSAGE=$4
 TO_ADDRESS=$5
 SUBJECT=$6
 
-FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/SERVER_IP/$SERVER_IP" -e "s/MESSAGE/$MESSAGE" template.html)
+FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/SERVER_IP/$SERVER_IP" -e "s/MESSAGE/$MESSAGE/g" template.html)
 
 {
 echo "To: $TO_ADDRESS"
