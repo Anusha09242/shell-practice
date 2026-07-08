cf4e0030c9a1ca425fdf8fee4aaef82c5e5524b4
Anusha09242 <anushapothu302@gmail.com>
2026-07-08 11:53:56 +0530

    Disk usage alert

--- 21.disk_usage.sh
+++ 21.disk_usage.sh
@@ -2,6 +2,7 @@
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 USAGE_THRESHOLD=10
+SERVER_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 while IFS= read -r line
 do
@@ -13,3 +14,5 @@ do
 done <<< "$DISK_USAGE"
 
 echo -e "$MESSAGE"
+
+sh mail.sh "DevOps Team" "High_Disk Usage" "$SERVER_IP" "$MESSAGE" "anushapothu690@gmail.com" "High Disk Usage Alert"
